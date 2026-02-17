571fcf223c134051b872bc7a919e278792abc471
Joeyovey <josephovey6@gmail.com>
2026-02-17 03:33:27 +0100

    fetching the data from the json server

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const renderPosts = async () =>{
+    let url = 'http://localhost:3000/posts'
+
+    const res = await fetch(url)
+    const posts = await res.json()
+
+    // console.log(posts)
+}
+
+window.addEventListener('DOMContentLoaded', () => renderPosts())
