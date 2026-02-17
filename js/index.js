b90558d15a9976a2b45197b3e95ad982e4699204
Joeyovey <josephovey6@gmail.com>
2026-02-17 04:10:12 +0100

    Displaying data on the DOM

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const container = document.querySelector('.blogs')
+
 const renderPosts = async () =>{
     let url = 'http://localhost:3000/posts'
 
@@ -5,6 +7,20 @@ const renderPosts = async () =>{
     const posts = await res.json()
 
     // console.log(posts)
+
+    let templeate = ''
+    posts.forEach(element => {
+        templeate += `
+            <div  class="post">
+                <h2>${element.title}</h2>
+                <p><small>${element.likes}  likes </small></p>
+                <p>${element.body.slice(0, 100)}</p>
+                <a href="/details.html">Read more...</a>
+            </div>
+        `
+    });
+
+    container.innerHTML = templeate
 }
 
 window.addEventListener('DOMContentLoaded', () => renderPosts())
