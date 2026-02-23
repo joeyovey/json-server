471d7b5b4ac5276bdd1b8a26c341401cd98a67da
Joeyovey <josephovey6@gmail.com>
2026-02-23 03:44:26 +0100

    Making a POST request to the Json server

---
+++ js/create.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('form');
+
+const createPost = async (e) => {
+    e.preventDefault();
+
+    const newPost = {
+        title: form.title.value,
+        body: form.body.value,
+        likes: 0
+    }
+    await fetch('http://localhost:3000/posts', {
+        method: 'POST',
+        body: JSON.stringify(newPost),
+        headers:{'Content-Type': 'applicaiont/json'}
+
+    })
+    window.location.replace('/index.html')
+}
+
+form.addEventListener('submit', createPost)
