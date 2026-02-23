8d4e45984af4cdc1041dbbd73de1d0115a85960a
Joeyovey <josephovey6@gmail.com>
2026-02-23 03:02:32 +0100

    sorting by likes in ascending order.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('.blogs')
 
 const renderPosts = async () =>{
-    let url = 'http://localhost:3000/posts'
+    let url = 'http://localhost:3000/posts?_sort=likes'
 
     const res = await fetch(url)
     const posts = await res.json()
